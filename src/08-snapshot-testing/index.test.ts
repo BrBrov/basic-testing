5f8971194b24dc630235cfdb238bbf265ad995d4
BrBrov <epoc@list.ru>
2023-07-01 21:56:32 +0300

    feat: create 8 task

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,38 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
     // Write your test here
+    const element = 1;
+
+    const expected = {
+      value: element,
+      next: {
+        value: null,
+        next: null
+      }
+    }
+    
+    const result = generateLinkedList([element]);
+    expect(result).toStrictEqual(expected);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
     // Write your test here
+    const element = 1;
+
+    const expected = {
+      value: element,
+      next: {
+        value: null,
+        next: null
+      }
+    }
+    
+    const result = generateLinkedList([element]);
+    expect(result).toMatchSnapshot(expected);
   });
 });
